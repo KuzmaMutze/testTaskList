dad21695235a517d912b68970f135fac6ecd0089
Matvey Fedorov <r3dw0r13@yandex.ru>
2022-02-09 07:00:32 +0400

    fix reduxtoolkit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose } from 'redux';
 import { appReducer } from './reducers/appReducer';
 
 let rootReducers = combineReducers({
